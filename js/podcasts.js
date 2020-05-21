9bace8e33839e2891544b77a0cba6b72bd4b0984
F4813787 Joao Carlos de Lucena Lira <joaocllira@bb.com.br>
2020-05-20 21:36:36 -0300

    Primeiro commit

---
+++ js/podcasts.js
@@ -0,0 +1,26 @@
+var podcasts = [
+    {
+        id: 1,
+        title: "Podcast AAA",
+        description: "Esse eh o Podcast AAA",
+        frequency: "90.5",
+    },
+    {
+        id: 2,
+        title: "Podcast BBB",
+        description: "Esse eh o Podcast BBB",
+        frequency: "93.6",
+    },
+    {
+        id: 3,
+        title: "Podcast CCC",
+        description: "Esse eh o Podcast CCC",
+        frequency: "99.8",
+    },
+    {
+        id: 4,
+        title: "Podcast DDD",
+        description: "Esse eh o Podcast DDD",
+        frequency: "101.2",
+    },
+];
---
+++ js/script.js
@@ -0,0 +1,58 @@
+window.addEventListener('load', start);
+
+var podcast;
+var $;
+
+function start() {
+    podcast = null;
+    $ = document.getElementById.bind(document);
+
+    document.getElementById('frequency').addEventListener('input', function(event) {
+        var valor = event.target.value;
+        document.getElementById('frequencyText').value = valor;
+        checkPodcast(valor);
+    });
+}
+
+function checkPodcast(valor) {
+    var podTemp = podcasts.find(tempPodcast => tempPodcast.frequency === valor);
+    if (podTemp) {
+        if (podcast === null) {
+            toggleVisibility($('notPodcast'), $('foundPodcast'));
+
+            changePodcast(podTemp);
+        } 
+    } else {
+        if (podcast !== null) {
+            podcast = null;
+
+            toggleVisibility($('foundPodcast'), $('notPodcast'));
+        }
+    }
+}
+
+function toggleVisibility(elementOn, elementOf) {
+    if (elementOn.classList.contains('visible')) {
+
+        elementOn.classList.replace('visible', 'invisible');
+        elementOf.classList.replace('invisible', 'visible');
+    } else {
+        elementOn.classList.replace('invisible', 'visible');
+        elementOf.classList.replace('visible', 'invisible');
+    }
+}
+
+function changePodcast(selectedPodcast) {
+    podcast = selectedPodcast;
+    
+    replaceValue($('title'), `[${podcast.frequency}] ${podcast.title}`);
+    replaceValue($('description'), podcast.description);
+
+    $('image').src = `./img/${podcast.id}.png`;
+}
+
+function replaceValue(input, newValue) {
+    input.firstChild.remove();
+    input.append(newValue);
+}
+
